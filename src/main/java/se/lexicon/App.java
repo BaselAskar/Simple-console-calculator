2dd623744b74d27cca87112f66faf65b426df5b2
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-10-07 11:54:06 +0200

    initial project

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+/**
+ * Simple-calculator
+ * @author Basel-Askar
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println("Simple-calculator");
+    }
+
+}
